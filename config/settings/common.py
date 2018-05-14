96a35cb1ff63b6002822269bc5680a4f755390a2
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-05-13 21:01:03 -0500

    ADD grapelli tittle

--- config/settings/common.py
+++ config/settings/common.py
@@ -169,3 +169,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER"),
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD"),
 EMAIL_PORT = os.environ.get("EMAIL_PORT", 584),
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", True),
+
+
+# Grappelli admin title
+GRAPPELLI_ADMIN_TITLE = "Bioservice"
